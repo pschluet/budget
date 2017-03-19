4bd908b344b851e0d9f9e92a7904015bb8ac9566
Paul Schlueter <paul@paulschlueter.com>
2017-03-19 00:39:05 -0500

    Initial Local Form Entry Working

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php 
+        include "populateFormFromDb.php"; 
+        date_default_timezone_set("America/Chicago");
+    ?>
+    <meta charset="UTF-8">
+    <title>Transaction Form</title>
+</head>
+<body>
+<form action="insert.php" method="post">
+    <p>
+        <label for="date_entry">Date</label>
+        <?php
+            $today = date('Y-m-d');
+            echo "<input type='date' name='date' id='date_entry' value='{$today}'>"
+        ?>        
+    </p>
+    <p>
+        <label for="category_entry">Category</label>
+        <select name="category" id="category_entry">
+            <?php
+                $dbm = new SqlDataManager();
+                $opt = $dbm->sqlQuery("SELECT * FROM categories ORDER BY names ASC");
+                printArrayAsFormOptions($opt);
+            ?>
+        </select>
+    </p>    
+    <p>
+        <label for="store_name_dropdown">Store Name</label>
+        <select name="storeNameDropdown" id="store_name_dropdown">
+            <?php
+                $opt = $dbm->sqlQuery("SELECT * FROM stores ORDER BY names ASC");
+                echo "<option value=''></option>\n";
+                printArrayAsFormOptions($opt);
+            ?>
+        </select>
+        <input type="text" name="storeNameTextbox" id="store_name_textbox">
+    </p>
+    <p>
+        <label for="description_entry">Description</label>
+        <input type="text" name="description" id="description_entry">
+    </p>
+    <p>
+        <fieldset>
+            <legend>Type</legend>
+            <label for="withdrawal_entry">Withdrawal</label>
+            <input type="radio" name="transType" id="withdrawal" value="withdrawal" checked="withdrawal"><br>
+            <label for="female">Deposit</label>
+            <input type="radio" name="transType" id="deposit" value="deposit"><br>
+        </fieldset>
+    </p>
+    <p>
+        <label for="split_entry">Split Transaction</label>
+        <input type="checkbox" name="split" id="split_entry">
+    </p>
+    <p>
+        <label for="amount_entry">Amount $</label>
+        <input type="number" name="amount" id="amount_entry">
+    </p>
+    <input type="submit" value="Submit">
+</form>
+</body>
+</html>
---
+++ insert.php
@@ -0,0 +1,36 @@
+<!-- Handle Form Submission -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Form Submission</title>
+</head>
+<body>
+<?php
+
+date_default_timezone_set("America/Chicago");
+include "populateFormFromDb.php";
+
+$dbm = new SqlDataManager();
+
+$dataToInsert = array(
+    "date" => $_REQUEST["date"],
+    "category" => $_REQUEST["category"],
+    "storeName" => (empty($_REQUEST["storeNameDropdown"]) ? $_REQUEST["storeNameTextbox"] : $_REQUEST["storeNameDropdown"]),
+    "description" => $_REQUEST["description"],
+    "isWithdrawal" => ($_REQUEST["transType"] == "withdrawal" ? true : false),
+    "isSplitTrans" => (array_key_exists("split", $_REQUEST) ? true : false),
+    "amount" => $_REQUEST["amount"]
+);
+if (!empty($dataToInsert["amount"]) && !empty($dataToInsert["description"])) {
+    if ($dbm->insertIntoTable("transactions", $dataToInsert)) {
+        echo "<h1>Success!</h1>";
+    } else {
+        echo "<h1>Fail!</h1>";
+    }
+} else {
+    echo "Please fill in at least description and amount.";
+}
+?>
+</body>
+</html>

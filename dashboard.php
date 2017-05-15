39429e073f5873eb1a1c05f508dec5b1dfe2d73b
Paul Schlueter <paul@paulschlueter.com>
2017-05-15 09:16:17 -0500

    Updated Library Includes + Example Plot

--- dashboard.php
+++ dashboard.php
@@ -13,15 +13,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Include jQuery Mobile stylesheets -->
-    <link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+    <!-- <link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css"> -->
+    <link rel="stylesheet" href="lib/jquery/jquery.mobile-1.4.5.min.css">
 
     <!-- Include the jQuery library -->
-    <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+    <!-- <script> src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
+    <script src="lib/jquery-3.2.1.min.js"></script>
+
 
     <!-- Include the jQuery Mobile library -->
-    <script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+    <!-- <script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>--> 
+    <script src="lib/jquery/jquery.mobile-1.4.5.min.js"></script>
     <meta charset="UTF-8">
-    <title>Data View</title>
+
+    <!-- Include Chart.js library -->
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script> -->
+    <script src="lib/Chart.min.js"></script>
+    <title>Dashboard</title>
 </head>
 <body>
 <div data-role="page">
@@ -33,6 +41,46 @@
         </ul>
     </div>
     <h1>Budget Categories</h1>
+    <canvas id="budgetChart" width="50" height="10"></canvas>
+    <script>
+        var ctx = $("#budgetChart");
+        var myChart = new Chart(ctx, {
+            type: 'horizontalBar',
+            data: {
+                labels: ["Baby", "House", "Car Insurance", "Groceries", "Food Out", "Gas"],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 10, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        });
+    </script>
 </div>
 </body>
 </html>

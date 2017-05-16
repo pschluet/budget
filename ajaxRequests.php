19c18d34962fd20bba153937f3df4bc5476fcd26
Paul Schlueter <paul@paulschlueter.com>
2017-05-16 13:28:50 -0700

    Plotting Data from AJAX Request + Updated JQuery Include Versions

--- ajaxRequests.php
+++ ajaxRequests.php
@@ -3,6 +3,7 @@
 include_once("utils.php");
 include_once("ensureLoggedIn.php"); 
 
+// Handle deleting a row from the transactions table
 if(isset($_POST["deleteId"])) {
 	$dbm = new SqlDataManager();
 	$deleteId = $dbm->secureFormInputText($_POST["deleteId"]);
@@ -14,8 +15,23 @@ if(isset($_POST["deleteId"])) {
 	} else {
 		echo "fail";
 	}
-} else {
-	echo "fail";
+}
+
+// Get data for the budget category plot
+if (isset($_POST["chartType"]) && $_POST["chartType"] == "categoryBar") {
+	$dbm = new SqlDataManager();
+	$sql = "SELECT
+                c.names as category,
+                SUM(t.amount) as amount
+            FROM transactions AS t
+            INNER JOIN 
+                categories AS c ON t.categoryId=c.id
+            GROUP BY category
+            ORDER BY category";
+
+	$res = $dbm->sqlQuery($sql);
+
+	echo json_encode($res);
 }
 
 ?>
--- dashboard.php
+++ dashboard.php
@@ -13,22 +13,78 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Include jQuery Mobile stylesheets -->
-    <!-- <link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css"> -->
-    <link rel="stylesheet" href="lib/jquery/jquery.mobile-1.4.5.min.css">
-
+    <link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+<!--     <link rel="stylesheet" href="lib/jquery/jquery.mobile-1.4.5.min.css"> -->
     <!-- Include the jQuery library -->
-    <!-- <script> src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
-    <script src="lib/jquery-3.2.1.min.js"></script>
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script> <!-- 3.2.1 -->
+<!--     <script src="lib/jquery-3.2.1.min.js"></script> -->
 
     <!-- Include the jQuery Mobile library -->
-    <!-- <script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>--> 
-    <script src="lib/jquery/jquery.mobile-1.4.5.min.js"></script>
+    <script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script> 
+<!--     <script src="lib/jquery/jquery.mobile-1.4.5.min.js"></script> -->    
     <meta charset="UTF-8">
 
     <!-- Include Chart.js library -->
-    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script> -->
-    <script src="lib/Chart.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
+<!--     <script src="lib/Chart.min.js"></script> -->
+    <script>
+        $(document).ready(function() {
+            $.ajax({
+                type:"POST",
+                url:"ajaxRequests.php",
+                data:{chartType: 'categoryBar'},
+                success:function(dataString) {
+                    // Parse data
+                    var data = JSON.parse(dataString);
+                    var categories = $.map(data, function(val, ii) {
+                        return val.category;
+                    });
+                    var amounts = $.map(data, function(val, ii) {
+                        return +val.amount;
+                    });
+
+                    // Make chart
+                    var ctx = $("#budgetChart");
+                    var myChart = new Chart(ctx, {
+                        type: 'horizontalBar',
+                        data: {
+                            labels: categories,
+                            datasets: [{
+                                label: 'Total Spent',
+                                data: amounts,
+                                backgroundColor: [
+                                    'rgba(255, 99, 132, 0.2)',
+                                    'rgba(54, 162, 235, 0.2)',
+                                    'rgba(255, 206, 86, 0.2)',
+                                    'rgba(75, 192, 192, 0.2)',
+                                    'rgba(153, 102, 255, 0.2)',
+                                    'rgba(255, 159, 64, 0.2)'
+                                ],
+                                borderColor: [
+                                    'rgba(255,99,132,1)',
+                                    'rgba(54, 162, 235, 1)',
+                                    'rgba(255, 206, 86, 1)',
+                                    'rgba(75, 192, 192, 1)',
+                                    'rgba(153, 102, 255, 1)',
+                                    'rgba(255, 159, 64, 1)'
+                                ],
+                                borderWidth: 1
+                            }]
+                        },
+                        options: {
+                            scales: {
+                                yAxes: [{
+                                    ticks: {
+                                        beginAtZero:true
+                                    }
+                                }]
+                            }
+                        }
+                    });                    
+                }
+            })
+        });
+    </script>
     <title>Dashboard</title>
 </head>
 <body>
@@ -41,45 +97,9 @@
         </ul>
     </div>
     <h1>Budget Categories</h1>
-    <canvas id="budgetChart" width="50" height="10"></canvas>
+    <canvas id="budgetChart" width="50" height="20"></canvas>
     <script>
-        var ctx = $("#budgetChart");
-        var myChart = new Chart(ctx, {
-            type: 'horizontalBar',
-            data: {
-                labels: ["Baby", "House", "Car Insurance", "Groceries", "Food Out", "Gas"],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 10, 3],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255,99,132,1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero:true
-                        }
-                    }]
-                }
-            }
-        });
+
     </script>
 </div>
 </body>
